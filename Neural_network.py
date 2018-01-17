4603d58dd42918e77e3a2ce23cc883426e04b663
Tushar Singh <tusharsingh62@gmail.com>
2018-01-17 20:42:31 +0530

    Create Neural_network.py

---
+++ Neural_network.py
@@ -0,0 +1,78 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import cv2
+from tqdm import tqdm
+import random
+
+df = pd.read_csv('labels.csv')
+df.head()
+
+n = len(df)
+gun = set(df['gun'])
+n_class = len(gun)
+class_to_num = dict(zip(gun, range(n_class)))
+num_to_class = dict(zip(range(n_class), gun))
+
+width = 299
+X = np.zeros((n, width, width, 3), dtype=np.uint8)
+y = np.zeros((n, n_class), dtype=np.uint8)
+for i in tqdm(range(n)):
+    X[i] = cv2.resize(cv2.imread('train/%s.png' % df['id'][i]), (width, width))
+    y[i][class_to_num[df['gun'][i]]] = 1
+
+
+from keras.layers import *
+from keras.models import *
+from keras.applications import *
+from keras.optimizers import *
+from keras.regularizers import *
+from keras.applications.inception_v3 import preprocess_input
+
+def get_features(MODEL, data=X):
+    cnn_model = MODEL(include_top=False, input_shape=(width, width, 3), weights='imagenet')
+    
+    inputs = Input((width, width, 3))
+    x = inputs
+    x = Lambda(preprocess_input, name='preprocessing')(x)
+    x = cnn_model(x)
+    x = GlobalAveragePooling2D()(x)
+    cnn_model = Model(inputs, x)
+
+    features = cnn_model.predict(data, batch_size=64, verbose=1)
+    return features
+
+inception_features = get_features(InceptionV3, X)
+xception_features = get_features(Xception, X)
+features = np.concatenate([inception_features, xception_features], axis=-1)
+
+inputs = Input(features.shape[1:])
+x = inputs
+x = Dropout(0.5)(x)
+x = Dense(n_class, activation='softmax')(x)
+model = Model(inputs, x)
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+h = model.fit(features, y, batch_size=128, epochs=100, validation_split=0.1)
+
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+
+SVG(model_to_dot(model, show_shapes=True).create(prog='dot', format='svg'))
+
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 2, 1)
+plt.plot(h.history['loss'])
+plt.plot(h.history['val_loss'])
+plt.legend(['loss', 'val_loss'])
+plt.ylabel('loss')
+plt.xlabel('epoch')
+
+plt.subplot(1, 2, 2)
+plt.plot(h.history['acc'])
+plt.plot(h.history['val_acc'])
+plt.legend(['acc', 'val_acc'])
+plt.ylabel('acc')
+plt.xlabel('epoch')
